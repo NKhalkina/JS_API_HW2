3e1e5f91aecbe52538a586586eb420005c1459e2
Tasha <tashik2010@mail.ru>
2024-06-02 20:24:10 +0300

    HW2

---
+++ slider.js
@@ -0,0 +1,42 @@
+//ловим элементы
+const sliderImages = document.querySelectorAll('.slider__img');
+const nextButtonEl = document.querySelector('.slider__next');
+const prevButtonEl = document.querySelector('.slider__prev');
+const paginationEl = document.querySelector('.pagination');
+const paginationCircles = document.querySelectorAll('.pagination__circle');
+let currentIndex = 1;
+
+updateClass(currentIndex, sliderImages, 'hidden');
+updateClass(currentIndex, paginationCircles, 'inactive');
+
+setInterval(() => {
+    updateClass(currentIndex, sliderImages, 'hidden');
+    updateClass(currentIndex, paginationCircles, 'inactive');
+    currentIndex = (currentIndex + 1) % sliderImages.length;
+}, 2000);
+
+nextButtonEl.addEventListener('click', showNextSlide);
+prevButtonEl.addEventListener('click', showPreviousSlide);
+paginationEl.addEventListener('click', ({ target }) => {
+    if (target.classList.contains('pagination__circle')) {
+        currentIndex = Number(target.getAttribute('data-index'));
+        updateClass(currentIndex, sliderImages, 'hidden');
+        updateClass(currentIndex, paginationCircles, 'inactive');
+    }
+});
+
+function updateClass(currentIndex, elements, className) {
+    elements.forEach((el, index) => el.classList.toggle(className, index !== currentIndex))
+}
+
+function showPreviousSlide() {
+    currentIndex = (currentIndex - 1 + sliderImages.length) % sliderImages.length;
+    updateClass(currentIndex, sliderImages, 'hidden');
+    updateClass(currentIndex, paginationCircles, 'inactive');
+}
+
+function showNextSlide() {
+    currentIndex = (currentIndex + 1) % sliderImages.length;
+    updateClass(currentIndex, sliderImages, 'hidden');
+    updateClass(currentIndex, paginationCircles, 'inactive');
+}
